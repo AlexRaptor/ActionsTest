14d75d12c6ad2c21d89964423d7517956dc1f861
Alexander Selivanov <a.selivanov@cft.ru>
2021-03-11 00:22:15 +0700

    Add test samples

---
+++ ActionsTest/Manager.swift
@@ -0,0 +1,24 @@
+//
+//  Manager.swift
+//  ActionsTest
+//
+//  Created by Селиванов Александр Владимирович on 11.03.2021.
+//
+
+import Foundation
+
+class Manager {
+
+    func fetchOne() -> Int {
+        1
+    }
+
+    func fetchTwo() -> Int {
+        2
+    }
+
+    func fetchThree() -> Int {
+        3
+    }
+
+}
--- ActionsTestTests/ActionsTestTests.swift
+++ ActionsTestTests/ActionsTestTests.swift
@@ -10,24 +10,25 @@ import XCTest
 
 class ActionsTestTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testOneIsOne() {
+        let manager = Manager()
+        let one = manager.fetchOne()
+
+        XCTAssertEqual(one, 1)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testTwoIsTwo() {
+        let manager = Manager()
+        let two = manager.fetchTwo()
+
+        XCTAssertEqual(two, 2)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testThree() {
+        let manager = Manager()
+        let three = manager.fetchThree()
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        XCTAssertEqual(three, 3)
     }
 
 }
