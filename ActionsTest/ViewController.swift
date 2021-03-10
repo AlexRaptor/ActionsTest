fbb08f70d86f01878739dadfcdd6992936d482aa
Alexander Selivanov <a.selivanov@cft.ru>
2021-03-10 23:58:12 +0700

    Initial Commit

---
+++ ActionsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ActionsTest
+//
+//  Created by Селиванов Александр Владимирович on 10.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
